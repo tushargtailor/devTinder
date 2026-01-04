36283e38c50853c4e97daed8db9369f6d5a98546
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-01-04 12:21:13 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Tushar",
-    lastName: "Tailor",
-  });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data succesfully sent to Database!!")
-})
-
-app.delete("/user", (req,res) => {
-  res.send("Successfully Deleted")
-})
-
-app.use("/test", (req, res) => {
-  res.send("Heloo welcome to my first nodejs test!!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("hello users whatsup.....!");
+    next()
+  },
+  (req, res,next) => {
+    // res.send("hello users whatsup.....!");
+    next()
+  },
+  (req, res) => {
+    res.send("hello users whatsup 3.....!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("succesfully server created on port 7777....");
