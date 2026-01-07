a291aa2d9ed047f6f3305c1310f388c9ae616030
Tushar Tailor <tushar.sdmcc.cs@gmail.com>
2026-01-07 14:51:19 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -4,19 +4,52 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  const users = await User.findOne({ emailId: userEmail });
+
+  try {
+    if (!users) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+
+  // const userEmail = req.body.emailId;
+
+  // const users = await User.find({ emailId: userEmail });
+
+  // try {
+  //   if (!users) {
+  //     res.status(404).send("something went wrong");
+  //   } else {
+  //     res.send(users);
+  //   }
+  // } catch (error) {
+  //   res.status(404).send("something went wrong");
+  // }
+});
+
+app.get("/feed", async (req, res) => {
+
+  const users = await User.find({});
+  res.send(users);
+})
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "lal",
-    lastName: "singh",
-    emailId: "lal@singh.com",
-    password: "lal@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
-    res.send("Data stored succesfully!!");
+    res.send("User added succesfully!!");
   } catch (error) {
-    res.status(401).send("unable to store user data");
+    res.status(400).send("unable to store user data");
   }
 });
 
